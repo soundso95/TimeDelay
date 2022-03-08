d059779cb05f76ea5433857c249d2c1cff2e873f
Andreas Vogler <l1622@schueler.ms-muc.de>
2022-03-08 10:07:51 +0100

    Lang-Lang TON-TOGGLE

--- TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
@@ -12,6 +12,7 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "StandardLib.h"
+#include "BasicLib.h"
 
 #define LOG_LOCAL_LEVEL ESP_LOG_INFO
 #include "esp_log.h"
@@ -20,6 +21,7 @@ static const char * const TAG = "EXAMPLE_TON";
 
 
 #define BUTTON_I1 GPIO_NUM_26        // Pin 26.
+#define BUTTON_I2 GPIO_NUM_32        // Pin 32.
 #define GPIO_Q1 GPIO_NUM_19            // Pin 19.
 
 
@@ -39,25 +41,38 @@ extern "C" void app_main(void)
     */
     gpio_reset_pin(GPIO_Q1);
     gpio_reset_pin(BUTTON_I1);
+    gpio_reset_pin(BUTTON_I2);
+
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
+    gpio_set_direction(BUTTON_I2, GPIO_MODE_INPUT);
+
     gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
+
 
 
     TON TON1;
     TON1.PT = 1000;
 
+    TOGGLE TOGGLE1;
+
+
     while (true) // Endlos-Schleife
     {
         // Eingang lesen, das not wird gebraucht weil die Eingaenge bei losgelassenem Taster auf 3.3V sind, und der Taster auf GND schaltet.
         bool I1 = not gpio_get_level(BUTTON_I1);
+        bool I2 = not gpio_get_level(BUTTON_I2);
+
 
         // den I1 an TON1 uebergeben, und TON1 aufrufen
         TON1(I1);
 
+        TOGGLE1.RST = I2;
+        TOGGLE1(TON1.Q);
+
         // Ausgaenge setzen
-        gpio_set_level(GPIO_Q1, TON1.Q);
+        gpio_set_level(GPIO_Q1,TOGGLE1.Q);
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
